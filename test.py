7ce80c064a6b2f1b6a01cb922397027329482701
Chebotarev-Alexey <66257265+Chebotarev-Alexey@users.noreply.github.com>
2022-12-01 11:41:35 +0200

    Fix import in test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from pytemplaterforpython import TemplatesLoader
+from pytemplatesforpython import TemplatesLoader
 tl = TemplatesLoader()
 tl.load_template("test_template.txt")
 print(repr(tl.get_template("test_template.txt").render(None)))
