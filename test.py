1c89e288abf45c1b9474fb7f825c0c4530bb3145
Chebotarev-Alexey <66257265+Chebotarev-Alexey@users.noreply.github.com>
2022-12-01 11:33:38 +0200

    Rename folder
    rename folder "pytemplater" to "pytemplatesforpython".

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from pytemplater import TemplatesLoader
+from pytemplaterforpython import TemplatesLoader
 tl = TemplatesLoader()
 tl.load_template("test_template.txt")
 print(repr(tl.get_template("test_template.txt").render(None)))
