cd16100cdc74e4637072e37a0acd4ccdd5d61508
Chebotarev-Alexey <66257265+Chebotarev-Alexey@users.noreply.github.com>
2022-12-01 21:03:58 +0200

    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
 	name="pytemplatesforpython",
-	version="1.1.0",
+	version="1.1.1",
 	author="Alexey-Chebotarev",
 	description="A package for useful python templates.",
 	long_description=long_description,
